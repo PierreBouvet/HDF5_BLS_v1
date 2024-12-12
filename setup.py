fad266e708ef38d50b6c951df7a2e8e705749abf
PierreBouvet <pierre.bouvet@meduniwien.ac.at>
2024-12-12 11:17:23 +0100

    Initialization 007

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup, find_packages
+import os
+
+# Helper function to read requirements.txt
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+# Read the requirements from requirements.txt
+requirements = parse_requirements('requirements.txt')
 
 setup(
-    name='my_project',
-    version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A description of your project.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/my_project',
+    name="HDF5_BLS",
+    version="0.1.0",
+    description="A package to read and write HDF5 files for the BioBrillouin lab",
+    author="Pierre Bouvet",
+    author_email="pierre.bouvet@meduniwien.ac.at",
     packages=find_packages(),
-    install_requires=[
-        # Add dependencies here
-    ],
+    install_requires=requirements,  # Use parsed requirements
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
